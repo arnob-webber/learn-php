8d8a250309889852153a25a9c549b3bdf2215899
arnob-webber <billahamid26@gmail.com>
2024-09-26 12:46:22 -0700

    php code

---
+++ associative-array.php
@@ -0,0 +1,23 @@
+<!-- an associative arraty is kinda of an object but it's not like that same same but different. it use sting as a key and valur -->
+
+
+<?php
+
+$webberMan = [
+
+   'name' => 'arnob',
+   'age' => 22
+   
+
+];
+// echo $webberMan['age']; <br>
+
+if ( $webberMan['age'] > 18)
+{
+    echo " he is arnob";
+}
+
+else {
+    echo 'he is not arnob';
+}
+?>

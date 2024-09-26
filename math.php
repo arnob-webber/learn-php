533a0b525aa8409248a46bbca1e250affe6590f4
arnob-webber <billahamid26@gmail.com>
2024-09-26 13:12:33 -0700

    gfg

---
+++ math.php
@@ -0,0 +1,20 @@
+<?php
+$a= 20;
+$b= 10;
+$c= 5;
+
+if ($a + $b > 0){
+    echo ++$c;
+
+    if( $c > 5)
+    {
+       while( $c === 6) {
+echo  " ". "arnob is love";
+$c++;
+       }
+    }
+}
+
+
+
+?>
